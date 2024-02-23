aa4258d3e4c23f1fcb50fcafb69fed39fb4d03a6
EllorAsh <Alicia.ellora04@gmail.com>
2024-02-23 11:38:07 +0200

    firts http test

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from "express"
 const app = express();
 
+app.get("/sign-up", (req, res)=>{
+    res.send("Hello");
+});
+
 app.listen(3000,()=>{
     console.log("server running on port 3000");
 });
