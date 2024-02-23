9d2f56b5168e38fc34b84c6667502e432a3f2d8f
EllorAsh <Alicia.ellora04@gmail.com>
2024-02-23 12:12:27 +0200

    http test

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 import express from "express"
-import {dirname} from "path";
-import {fileURLToPath} from "url";
 import bodyParser from "body-parser";
 const app = express();
-const_dirname = dirname(fileURLToPath(import.meta.url))
-
-app.use(bodyParser.urlencoded({extended:true}));
 
 app.post("/signup", (req, res)=>{
     res.send({ message: 'No blah Found' });
